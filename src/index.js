bf379563d074ba8ad3c14ff5f1977a8936c65f6b
faisal-work <faisal@rently.com>
2024-12-15 01:35:54 +0530

    added airbrake releated change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ErrorBoundary>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </ErrorBoundary>
+  // </React.StrictMode>
 );
